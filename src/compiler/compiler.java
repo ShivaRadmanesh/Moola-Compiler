0ed6c7a2dffbbb129e267fe7db6a92db5c397948
saharEslami <seslami2@mail.um.ac.ir>
2020-11-15 20:37:38 +0330

    compiler folder added

---
+++ src/compiler/compiler.java
@@ -0,0 +1,27 @@
+import gen.MoolaBaseListener;
+import gen.MoolaLexer;
+import gen.MoolaListener;
+import gen.MoolaParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
+public class compiler {
+    public static void main(String[] args) throws IOException {
+        CharStream stream = CharStreams.fromFileName("./sample/text.mla");
+        MoolaLexer lexer = new MoolaLexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        MoolaParser parser = new MoolaParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree tree = parser.program();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        MoolaListener listener = new MoolaBaseListener();
+        walker.walk(listener , tree);
+    }
+}
+
